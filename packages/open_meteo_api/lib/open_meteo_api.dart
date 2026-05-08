2ea2023d879a39c6217aa080063b392ec3dd5c11
agustinaaa0408 <207658721+agustinaaa0408@users.noreply.github.com>
2026-05-08 23:06:28 +0700

    flutter_weather

--- packages/open_meteo_api/lib/open_meteo_api.dart
+++ packages/open_meteo_api/lib/open_meteo_api.dart
@@ -1,5 +1,2 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/models/models.dart';
+export 'src/open_meteo_api_client.dart';
--- packages/open_meteo_api/test/open_meteo_api_test.dart
+++ packages/open_meteo_api/test/open_meteo_api_test.dart
@@ -1,12 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:open_meteo_api/open_meteo_api.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('simple test', () {
+    expect(1 + 1, 2);
   });
 }
