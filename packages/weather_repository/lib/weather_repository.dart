10bfd540a719fa709df901550d1945bedc06ca22
tika kartika <tikaputri469@gmail.com>
2026-05-12 11:02:52 +0700

    Update weather_repository.dart

--- packages/weather_repository/lib/weather_repository.dart
+++ packages/weather_repository/lib/weather_repository.dart
@@ -1,8 +1,4 @@
+library weather_repository;
 
 export 'src/models/models.dart';
 export 'src/weather_repository.dart';
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-} 
