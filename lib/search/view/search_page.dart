25ddbb9a6d2b39b25b2e594d86b42b799c05d07b
anissafitriramadhani <farrell.don00@gmail.com>
2026-05-15 17:19:32 +0700

    poles

--- lib/search/view/search_page.dart
+++ lib/search/view/search_page.dart
@@ -43,7 +43,13 @@ class _SearchPageState extends State<SearchPage> {
           IconButton(
             key: const Key('searchPage_search_iconButton'),
             icon: const Icon(Icons.search, semanticLabel: 'Submit'),
-            onPressed: () => Navigator.of(context).pop(_text),
+            onPressed: () {
+              final city = _textController.text.trim();
+
+              if (city.isEmpty) return;
+
+              Navigator.of(context).pop(city);
+            },
           ),
         ],
       ),
